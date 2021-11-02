4c831385b63e84c06800ca30c262435bda5f7aa5
Oderystella <ddott59@gmail.com>
2021-11-02 16:14:28 +0900

    feat: create Category model

--- postings/models.py
+++ postings/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from core.models import AbstractTimeStamped
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
 
 
-class Posting(models.Model):
+class Posting(AbstractTimeStamped):
     title = models.CharField(max_length=128, default="")
     text = models.TextField()
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     class Meta:
         db_table = "postings"
+
--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from core.models import AbstractTimeStamped
 
 
-class User(models.Model):
+class User(AbstractTimeStamped):
     name = models.CharField(max_length=32, null=True)
     email = models.CharField(max_length=64, unique=True)
     password = models.CharField(max_length=128, null=True)
