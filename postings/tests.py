4a19a464042509fc3f9d5f03c3c67d3578c0362c
Oderystella <ddott59@gmail.com>
2021-12-20 22:52:26 +0900

    feat: create PostingView, SearchView testcode
    - 게시글 상세 조회, 검색 기능에 대한 테스트 코드 작성
    - 뷰 코드 시간 형식 수정

--- postings/tests.py
+++ postings/tests.py
@@ -1,208 +1,121 @@
-import json
+import jwt
 
-from django.test import TestCase, Client
+from django.test      import TestCase, Client
+from django.db.models import Q
 
-from postings.models import Posting
-from users.models import User
+from postings.models import Posting, Category
+from users.models    import User
+from my_settings     import MY_SECRET_KEY, ALGORITHM
 
 
-class PostingCRUDTest(TestCase):
+class PostingView(TestCase):
     def setUp(self):
-        User.objects.create(id=1, name="Ted", email="hayejun1013@naver.com", password="abcd1234"),
-        User.objects.create(id=2, name="Ted", email="hayejun1013@naver.co.kr", password="abcd1234")
-        
-        Posting.objects.create(id=1, title="Title", text="text", author_id=1)
+        self.client = Client()
+
+        User.objects.create(id=1, name="Stella", email="jun10@naver.com", password="abcd1234"),
+
+        self.access_token1 = jwt.encode({"id": 1}, MY_SECRET_KEY, ALGORITHM)
+
+        Category.objects.create(id=1, name="python")
+
+        Posting.objects.bulk_create(
+            [
+                Posting(id=1, title="Title1", text="text", author_id=1, category_id=1),
+                Posting(id=2, title="Title1", text="text", author_id=1, category_id=1),
+            ]
+        )
 
     def tearDown(self):
         Posting.objects.all().delete()
+        Category.objects.all().delete()
         User.objects.all().delete()
 
-    # def test_get_posting_success(self):
-    #     client = Client()
-    #     response = client.get("/postings")
+    def test_get_posting_success(self):
+        header = {"HTTP_Authorization": f"Bearer {self.access_token1}"}
 
-    #     posting = Posting.objects.get(id=1)
+        response = self.client.get("/postings/1", content_type="application/json", **header)
 
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(
-    #         response.json(),
-    #         {
-    #             "result": {
-    #                 "id": posting.id,
-    #                 "author": posting.author.name,
-    #                 "title": posting.title,
-    #                 "text": posting.text,
-    #                 "created_time": posting.created_time.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z",
-    #                 "updated_at": posting.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]+ "Z",
-    #             }
-    #         },
-    #     )
+        posting = Posting.objects.get(id=1)
 
-    # def test_get_posting_not_found(self):
-    #     client = Client()
-    #     response = client.get("/postings")
-
-    #     self.assertEqual(response.status_code, 404)
-    #     self.assertEqual(response.json(), {"message": "POSTING_10_NOT_FOUND"})
-
-    # def test_create_post_success(self):
-    #     client = Client()
-    #     data = {
-    #         "title": "title",
-    #         "text": "text",
-    #     }
-    #     header = {
-    #         "HTTP_Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.DiY2YcXuu_5bwMbFqlTe_x4-0Msg6_Lmq3YVhsDr4LU"
-    #     }
-
-    #     response = client.post(
-    #         "/postings", json.dumps(data), content_type="application/json", **header
-    #     )
-
-    #     self.assertEqual(response.status_code, 201)
-    #     self.assertEqual(response.json(), {"message": "title has successfully posted"})
-
-    def test_create_post_auth_error(self):
-        client = Client()
-        data = {
-            "title": "title",
-            "text": "text",
-        }
-        header = {
-            "HTTP_Authorization": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.DiY2YcXuu_5bwMbFqlTe_x4-0Msg6_Lmq3YVhsDr4LU"
+        result = {
+            "id": posting.id,
+            "author": posting.author.name,
+            "title": posting.title,
+            "text": posting.text,
+            "category": posting.category.name,
+            "created_at": posting.created_at.strftime("%Y-%m-%d %H:%M"),
+            "updated_at": posting.updated_at.strftime("%Y-%m-%d %H:%M"),
+            "count": posting.count
         }
 
-        response = client.post(
-            "/postings", json.dumps(data), content_type="application/json", **header
-        )
-
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(response.json(), {"message": "AUTH_ERROR"})
-
-    def test_create_post_invalid_token(self):
-        client = Client()
-        data = {
-            "title": "title",
-            "text": "text",
-        }
-        header = {
-            "HTTP_Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.DiY2YcXuu_5bwMbFqlTe_x4-0Msg6_Lmq3YVhsDr4L"
-        }
-
-        response = client.post(
-            "/postings", json.dumps(data), content_type="application/json", **header
-        )
-
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(response.json(), {"message": "INVALID_TOKEN"})
-
-    def test_create_post_key_error(self):
-        client = Client()
-        data = {
-            "title": "title",
-        }
-        header = {
-            "HTTP_Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.DiY2YcXuu_5bwMbFqlTe_x4-0Msg6_Lmq3YVhsDr4LU"
-        }
-
-        response = client.post(
-            "/postings", json.dumps(data), content_type="application/json", **header
-        )
-
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {"message": "KEY_ERROR"})
-
-    def test_put_post_success(self):
-        client = Client()
-        data = {
-            "text": "text updated!",
-        }
-        header = {
-            "HTTP_Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.DiY2YcXuu_5bwMbFqlTe_x4-0Msg6_Lmq3YVhsDr4LU"
-        }
-
-        response = client.put(
-            "/postings/1", json.dumps(data), content_type="application/json", **header
-        )
-
+        self.assertEqual(response.json(), {"result": result})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"message": "Title has successfully updated"})
 
-    def test_put_post_forbidden(self):
-        client = Client()
-        data = {
-            "text": "text updated!",
-        }
-        header = {
-            "HTTP_Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Mn0.OLjKxY1ipx6vU8RSSkPtwE_d-0S9_qMVPg9syryaDQA"
-        }
+    def test_get_posting_not_found(self):
+        header = {"HTTP_Authorization": f"Bearer {self.access_token1}"}
 
-        response = client.put(
-            "/postings/1", json.dumps(data), content_type="application/json", **header
+        posting_id = 3
+        response = self.client.get(f"/postings/{posting_id}", content_type="application/json", **header)
+        
+        self.assertEqual(response.json(), {"message" : f"POSTING_{posting_id}_NOT_FOUND"})
+        self.assertEqual(response.status_code, 404)
+
+
+class SearchView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        User.objects.create(id=1, name="Stella", email="jun10@naver.com", password="abcd1234"),
+        User.objects.create(id=2, name="Stella", email="jun10@naver.co.kr", password="abcd1234")
+        
+        Category.objects.create(id=1, name="python")
+        Category.objects.create(id=2, name="mysql")
+
+        Posting.objects.bulk_create(
+            [
+                Posting(id=1, title="Title1", text="text", author_id=1, category_id=1),
+                Posting(id=2, title="Title1", text="text", author_id=1, category_id=1),
+                Posting(id=3, title="Title3", text="text", author_id=2, category_id=2),
+                Posting(id=4, title="Title4", text="text4", author_id=2, category_id=2),
+                Posting(id=5, title="Title5", text="text5", author_id=1, category_id=2),
+            ]
         )
 
-        self.assertEqual(response.status_code, 403)
-        self.assertEqual(response.json(), {"message": "FORBIDDEN"})
+    def tearDown(self):
+        Posting.objects.all().delete()
+        Category.objects.all().delete()
+        User.objects.all().delete()
 
-    def test_put_post_key_error(self):
-        client = Client()
-        data = {}
-        header = {
-            "HTTP_Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.DiY2YcXuu_5bwMbFqlTe_x4-0Msg6_Lmq3YVhsDr4LU"
+    def test_get_search_success(self):
+        keyword = "Title1"
+
+        response = self.client.get(f"/postings/search?keyword={keyword}", content_type="application/json")
+
+        postings = Posting.objects.filter(Q(author__name=keyword)|Q(title__icontains=keyword)|Q(text__icontains=keyword))
+
+        result = {
+            "count": len(postings),
+            "postings": [
+                {
+                    "id": posting.id,
+                    "author": posting.author.name,
+                    "title": posting.title,
+                    "text": posting.text,
+                    "category": posting.category.name,
+                    "created_at": posting.created_at.strftime("%Y-%m-%d %H:%M"),
+                    "updated_at": posting.updated_at.strftime("%Y-%m-%d %H:%M"),
+                }
+                for posting in postings
+            ]
         }
 
-        response = client.put(
-            "/postings/1", json.dumps(data), content_type="application/json", **header
-        )
+        self.assertEqual(response.json(), {"result": result})
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_posting_not_found(self):
+        keyword = "post"
+        
+        response = self.client.get(f"/postings/search?keyword={keyword}", content_type="application/json")
 
+        self.assertEqual(response.json(), {"message" : "NOT_FOUND_POSTING"})
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {"message": "KEY_ERROR"})
-
-    def test_delete_post_success(self):
-        client = Client()
-
-        header = {
-            "HTTP_Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.DiY2YcXuu_5bwMbFqlTe_x4-0Msg6_Lmq3YVhsDr4LU"
-        }
-
-        response = client.delete("/postings/1", **header)
-
-        self.assertEqual(response.status_code, 204)
-
-    def test_delete_post_forbidden(self):
-        client = Client()
-
-        header = {
-            "HTTP_Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Mn0.OLjKxY1ipx6vU8RSSkPtwE_d-0S9_qMVPg9syryaDQA"
-        }
-
-        response = client.put("/postings/1", **header)
-
-        self.assertEqual(response.status_code, 403)
-        self.assertEqual(response.json(), {"message": "FORBIDDEN"})
-
-    # def test_get_post_list_success(self):
-    #     client = Client()
-
-    #     response = client.get("/postings/list")
-
-    #     posting = Posting.objects.get(id=1)
-
-    #     result = {
-    #         "result": {
-    #             "count": 1,
-    #             "postings": [
-    #                 {
-    #                     "id": posting.id,
-    #                     "author": posting.author.name,
-    #                     "title": posting.title,
-    #                     "text": posting.text,
-    #                     "created_time": posting.created_time.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z",
-    #                     "updated_at": posting.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z",
-    #                 }
-    #             ],
-    #         }
-    #     }
-
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.json(), result)
